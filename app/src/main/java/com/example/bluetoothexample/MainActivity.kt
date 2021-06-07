d86cb80096d0395c3b6c8ff295a6b816e77646aa
pablopsp <pablosilio@gmail.com>
2021-06-07 15:54:37 +0200

    Message send/receive working 🥺

--- app/src/main/java/com/example/bluetoothexample/MainActivity.kt
+++ app/src/main/java/com/example/bluetoothexample/MainActivity.kt
@@ -4,69 +4,41 @@ import android.bluetooth.BluetoothAdapter
 import android.bluetooth.BluetoothDevice
 import android.bluetooth.BluetoothServerSocket
 import android.bluetooth.BluetoothSocket
-import android.content.BroadcastReceiver
-import android.content.Context
-import android.content.Intent
-import android.content.IntentFilter
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import android.util.Log
 import android.widget.Button
 import android.widget.Toast
-import java.io.*
+import java.io.IOException
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var blDevice: BluetoothDevice
     private var btAdapter: BluetoothAdapter? = null
-    
-    private val receiver = object : BroadcastReceiver() {
-        override fun onReceive(context: Context, intent: Intent) {
-            when (intent.action) {
-                BluetoothDevice.ACTION_FOUND -> {
-                    val device: BluetoothDevice? = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE)
-                    val deviceName = device?.name
-                    Log.i(TAG, deviceName.toString())
-                    if (device != null && deviceName.equals("HUAWEI Y7")) {
-                        Log.i(TAG, "encontrado device")
-                        blDevice = device
-                        btAdapter?.cancelDiscovery()
-                    }
-                    
-                    Log.i("Bl", deviceName.toString())
-                }
-            }
-        }
-    }
     
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         
-        val filter = IntentFilter(BluetoothDevice.ACTION_FOUND)
-        registerReceiver(receiver, filter)
+        val mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
+        val pairedDevices = mBluetoothAdapter.bondedDevices
+        
+        val myDevice = pairedDevices.filter {
+            it.name == "HUAWEI Y7"
+        }
         
         btAdapter = BluetoothAdapter.getDefaultAdapter()
-        btAdapter?.startDiscovery()
         
         findViewById<Button>(R.id.cliente).setOnClickListener {
-            if (this::blDevice.isInitialized) {
-                BluetoothClient(blDevice).start()
-                Log.i(TAG, "init client")
-            }
+            BluetoothClient(myDevice[0]).start()
+            Log.i(TAG, "looking for devices")
         }
         
         findViewById<Button>(R.id.servidor).setOnClickListener {
-            btAdapter?.cancelDiscovery()
             BluetoothServerController(this).start()
             Log.i(TAG, "init server")
         }
     }
     
-    override fun onDestroy() {
-        super.onDestroy()
-        unregisterReceiver(receiver)
-    }
     
     private inner class BluetoothServerController(private val activity: MainActivity) : Thread() {
         private var cancelled: Boolean
@@ -93,14 +65,14 @@ class MainActivity : AppCompatActivity() {
                 }
                 
                 try {
-                    socket = serverSocket!!.accept()  // 2
+                    socket = serverSocket!!.accept()
                 } catch (e: IOException) {
                     break
                 }
                 
                 if (!this.cancelled && socket != null) {
                     Log.i("server", "Connecting")
-                    BluetoothServer(activity, socket).start() // 3
+                    BluetoothServer(activity, socket).start()
                 }
             }
         }
@@ -114,28 +86,24 @@ class MainActivity : AppCompatActivity() {
     private inner class BluetoothServer(private val activity: MainActivity, private val socket: BluetoothSocket) :
         Thread() {
         private val inputStream = this.socket.inputStream
-        private val outputStream = this.socket.outputStream
         
         override fun run() {
+            val buffer = ByteArray(1024)
+            Log.i(TAG, inputStream.available().toString())
+            
             try {
-                val available = inputStream.available()
-                val bytes = ByteArray(available)
-                
                 Log.i("server", "Reading")
-                inputStream.read(bytes, 0, available)
-                val text = String(bytes)
+                val bytes = inputStream.read(buffer)
+                val text = String(buffer, 0, bytes)
                 
                 Log.i("server", "Message received $text")
                 
                 activity.runOnUiThread {
                     Toast.makeText(activity, text, Toast.LENGTH_LONG).show()
                 }
+                
             } catch (e: Exception) {
                 Log.e("client", "Cannot read data", e)
-            } finally {
-                inputStream.close()
-                outputStream.close()
-                this.socket.close()
             }
         }
     }
@@ -144,23 +112,20 @@ class MainActivity : AppCompatActivity() {
         private val socket = device.createRfcommSocketToServiceRecord(myUUID)
         
         override fun run() {
+            btAdapter!!.cancelDiscovery()
             Log.i("client", "Connecting")
-            this.socket.connect()
+            socket.connect()
             
-            val inputStream = this.socket.inputStream
-            val outputStream = this.socket.outputStream
+            val outputStream = socket.outputStream
             
             Log.i("client", "Sending")
             try {
-                outputStream.write("Holaa".toByteArray())
+                outputStream.write("Hello".toByteArray())
                 outputStream.flush()
+                
                 Log.i("client", "Sent message")
             } catch (e: Exception) {
                 Log.e("client", "Cannot send", e)
-            } finally {
-                outputStream.close()
-                inputStream.close()
-                this.socket.close()
             }
         }
     }
